68a4fd2fe4d2f8abc977249b6f18b6a87c10465e
nicholasmullikin <nicholas.mullikin@gmail.com>
2016-11-09 10:17:24 -0500

    Files
    All the files

---
+++ answer_display.js
@@ -0,0 +1,49 @@
+var json = { "title": "Recovery Activities", "description": "", "questions": [ {"id":"1","question":"How long after your injury did you return to pratice or play of a sport?","type":"text", "units":"days"}, {"id":"2","question":"On average, how many hours per night do you sleep?","type":"blank"}, {"id":"3","question":"Before your concussion:","type":"text", "units":"hours"}, {"id":"4","question":"After your concussion:","type":"text", "units":"hours"}, {"id":"5","question":"Did you miss any school or work?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"6","question":"If yes, how many days(including half days)?","type":"text", "units":""}, {"id":"7","question":"Were you given work to complete? (i.e. schoolwork, homework, or work responsibilities)","type":"choice", "answers":["yes","no"], "units":""}, {"id":"8","question":"If yes, did you complete it?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"9","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"10","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"11","question":"Did you watch TV?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"12","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"13","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"14","question":"Did you use your cell phone for anything other than phone calls? (e.g. texting, social media, apps, etc.)","type":"choice", "answers":["yes","no"], "units":""}, {"id":"15","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"16","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"17","question":"Did you play video, phone, or computer games?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"18","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"19","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"20","question":"Did you use the computer for leisure?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"21","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"22","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"23","question":"Did you use your computer for work?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"24","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"25","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"26","question":"Did you do any leisurely reading? (e.g. books, magazines, etc.)","type":"choice", "answers":["yes","no"], "units":""}, {"id":"27","question":"If yes, what source did you use?","type":"choice", "answers":["Paper","Kindle e-Reader","Computer","iPad/Tablet"], "units":""}, {"id":"28","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"29","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"30","question":"Did you do any work-related reading? (e.g. homework, articles, research, etc.)","type":"choice", "answers":["yes","no"], "units":""}, {"id":"31","question":"If yes, what source did you use?","type":"choice", "answers":["Paper","Kindle e-Reader","Computer","iPad/Tablet"], "units":""}, {"id":"32","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"33","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"34","question":"Did you listen to audio books, talk radio, or podcasts?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"35","question":"If yes, did you use headphones?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"36","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"37","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"38","question":"Did you listen to music?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"39","question":"If yes, did you use headphones?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"40","question":"What type?","type":"choice", "answers":["Pop","Hip hop","Rock","Rap","Pop Rock", "R&B","Heavy metal","Electronic","Classical","OTHER"], "units":""}, {"id":"41","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"42","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"43","question":"Did friends come to visit?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"44","question":"If yes, how many at a time?","type":"text", "units":""}, {"id":"45","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"46","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"47","question":"Did you travel?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"48","question":"If yes, how long was your travel time?","type":"blank"}, {"id":"49","question":"To your destination:","type":"text", "units":"mins"}, {"id":"50","question":"From your destination:","type":"text", "units":"mins"}, {"id":"51","question":"Mode of transportation?","type":"choice", "answers":["Car","Bus","Train/subway","Plane"], "units":""}, {"id":"52","question":"Did you walk for more than 5 minutes per day?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"53","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"54","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"55","question":"Did you exercise? (e.g. running, swimming, biking, sports, treadmill, sexual intercourse, or other form of exercise)","type":"choice", "answers":["yes","no"], "units":""}, {"id":"56","question":"If yes, please specify:","type":"text", "units":""}, {"id":"57","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"58","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"59","question":"Did you carry heavy itmes during your recovery?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"60","question":"If yes, please list the itmes and estimate the weight:","type":"text", "units":""}, {"id":"61","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"62","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"63","question":"Did you do any household chores?","type":"choice", "answers":["yes","no"], "units":""}, {"id":"64","question":"If yes, what chores?","type":"choice", "answers":["Cleaning","Taking out the garbage","Laundry","Washing dishes","Cooking","Grocery shopping","Raking/shoveling","Mowing the lawn","OTHER"], "units":""}, {"id":"65","question":"How many hours per day?","type":"choice", "answers":["0.5","1","1.5","2","2.5","3","3.5","4","4.5","5","5.5","6+"], "units":""}, {"id":"66","question":"How many days per week?","type":"choice", "answers":["1","2","3","4","5","6","7"], "units":""}, {"id":"67","question":"What date did you consider yourself fully recovered?","type":"text", "units":""} ] };
+
+var el = document.getElementById("userChoice");
+
+for(var i = 0; i < json['questions'].length; i++){
+	var submit = "";
+	if(json['questions'][i].type === 'text'){
+		submit+= json['questions'][i].question;
+		if(localStorage.getItem("id:"+json['questions'][i].id) == ""){
+			submit+= " No Answer"
+		}
+		else{
+		submit+= " "+localStorage.getItem("id:"+json['questions'][i].id);
+		submit+= " "+json['questions'][i].units;}
+	}
+	else if(json['questions'][i].type === 'choice'){
+		submit+= json['questions'][i].question;
+		var answerLength = json['questions'][i].answers.length;
+		for(var j = 0; j<answerLength; j++){
+			submit+= '<input type="checkbox" disabled =""'
+			if (localStorage.getItem("id:"+json['questions'][i].id+","+json['questions'][i].answers[j]) == "true"){
+			submit+= 'checked'}
+			submit+= ">"
+			
+			if(json['questions'][i].answers[j] === 'OTHER'){
+				submit+='Other:';
+				if(localStorage.getItem("id:"+json['questions'][i].id) == ""){
+					submit+= " No Answer"
+				}
+				else{
+				submit+= " "+localStorage.getItem("id:"+json['questions'][i].id);
+				}
+			}
+			else{
+				submit+= json['questions'][i].answers[j];
+			}
+			
+		}
+		
+
+
+	}
+	else if(json['questions'][i].type === 'blank'){
+		submit+= json['questions'][i].question;
+	}
+	else{submit = 'An error has occured'};
+	el.innerHTML += '<br>';
+	el.innerHTML +=	submit;
+}
